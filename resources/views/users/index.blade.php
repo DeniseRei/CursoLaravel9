a3326febc88cef8f126c86ae0fca86e19078d504
DeniseRei <lopesdeniserei@gmail.com>
2022-04-29 14:06:44 -0300

    template blade estrutura

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,3 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem dos Usuários')
+
+@section('content')
+
 <h1>Listagem de Usuários</h1>
 
 
@@ -7,7 +13,8 @@
         {{ $user->name }} ->
         {{ $user->email }}
         | <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
-
     </li>
     @endforeach
 </ul>
+
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,7 +1,14 @@
-<h1>Listagem do Usuário {{ $user->name }}</h1>
+@extends('layouts.app')
+<!--LISTAGEM DO USUARIO -->
+@section('title', 'Dados sobre o Usuário' )
+
+@section('content')
+
+<h1>Dados sobre o Usuário - {{ $user->name }}</h1>
 
 <ul>
     <li>{{ $user->name }}</li>
     <li>{{$user->email}}</li>
 
 </ul>
+@endsection

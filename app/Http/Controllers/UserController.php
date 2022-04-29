dc3c64d286ffb18b2562ff68d0d58fbc756e03ab
DeniseRei <lopesdeniserei@gmail.com>
2022-04-28 23:28:40 -0300

    listar usuarios controller e blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
+        $users = User::all();
         //dd('UserController@index');
-        return view('users.index');
+        return view('users.index', compact('users'));
     }
 
     public function show($id)
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,2 +1,11 @@
-listagem dos usuários
-Dete Lindaaaaa
+<h1>Listagem de Usuários</h1>
+
+
+<ul>
+    @foreach ($users as $user)
+    <li>
+        {{ $user->name }} ->
+        {{ $user->email }}
+    </li>
+    @endforeach
+</ul>

1edc1e85402fc8d1b6b4f4f88ded16fc835ac119
DeniseRei <lopesdeniserei@gmail.com>
2022-04-28 22:23:49 -0300

    seederUsers

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => "Denise Lopes",
+            'email'=> "denise@mail.com",
+            'password'=> bcrypt('123456789'),
+        ]);
+    }
+}

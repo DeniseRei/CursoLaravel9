28e815520dacab153cce86598a08ccdccc2f1e8b
DeniseRei <lopesdeniserei@gmail.com>
2022-05-27 21:15:32 -0300

    sistema de login auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 use App\Http\Controllers\{
     UserController
 };
@@ -7,22 +18,26 @@ use App\Http\Controllers\Admin\CommentController;
 use Illuminate\Support\Facades\Route;
 
 /*As sequências das rotas tem que ser da seguinte forma*/
+Route::middleware(['auth'])->group(function(){
+    Route::get('users', [UserController::class, 'index'])->name('users.index');
+    Route::get('users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('users', [UserController::class, 'store'])->name('users.store');
+    Route::get('users/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
-Route::get('users', [UserController::class, 'index'])->name('users.index');
-Route::get('users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('users', [UserController::class, 'store'])->name('users.store');
-Route::get('users/{id}', [UserController::class, 'show'])->name('users.show');
-Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    /*Comments*/
+    Route::get('users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
+    Route::get('users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+    Route::post('users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::get('users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
+    Route::put('users/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
 
-/*Comments*/
-Route::get('users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
-Route::get('users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
-Route::post('users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::get('users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
-Route::put('users/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
-
-Route::get('/', function () {
-    return view('welcome');
 });
+
+   Route::get('/', function () {
+        return view('welcome');
+    });
+require __DIR__.'/auth.php';
+
